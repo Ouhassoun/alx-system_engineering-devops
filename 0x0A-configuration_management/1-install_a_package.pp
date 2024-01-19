21032fe57c8ec73fd7cd3a207d5a97ce14999468
Jaguar <gitibillyzombie@gmail.com>
2024-01-19 14:51:15 -0800

    meow

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,8 @@
+# to Use Puppet to install flask from pip3
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+}
+
